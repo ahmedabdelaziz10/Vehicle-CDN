3c77924c488ca5d84f8dcc9e579cdf3ee58c17f8
ahmedabdelaziz10 <125483891+ahmedabdelaziz10@users.noreply.github.com>
2024-04-03 00:32:44 +0300

    Update Simple.py

--- Simple.py
+++ Simple.py
@@ -11,7 +11,7 @@ from mn_wifi.vanet import vanet
 from mn_wifi.telemetry import telemetry
 from mn_wifi.sumo.runner import sumo
 
-def simple_file(args):
+def simple_file():
 
     # Create Network
     net = Mininet_wifi(controller=Controller, accessPoint=UserAP, link=wmediumd, wmediumd_mode=interference)
@@ -36,32 +36,26 @@ def simple_file(args):
     net.configureNodes()
     net.addLink(ap1,h1)
 
-    if '-p' not in args:
-        net.plotGraph(max_x=300, max_y=300)
+ 
+    net.plotGraph(max_x=300, max_y=300)
 
     # Set mobility model for stations
     net.startMobility(time=0, mob_rep=1, reverse=False)
 
     p1, p2, p3, p4 = {}, {}, {}, {}
-    if '-c' not in args:
-        p1 = {'position': '50.0,50.0,0.0'}
-        p2 = {'position': '40.0,40.0,0.0'}
-        p3 = {'position': '250.0,250,0.0'}
-        p4 = {'position': '289.0,31.0,0.0'}
+    p1 = {'position': '50.0,50.0,0.0'}
+    p2 = {'position': '40.0,40.0,0.0'}
+    p3 = {'position': '250.0,250,0.0'}
+    p4 = {'position': '289.0,31.0,0.0'}
 
-    while True:
     
-    	net.mobility(sta1, 'start', time=0, **p1)
-    	net.mobility(sta2, 'start', time=0, **p2)
-    	net.mobility(sta1, 'stop', time=25, **p3)
-    	net.mobility(sta2, 'stop', time=25, **p4)
-    	net.stopMobility(time=23)
-    	print(sta1.wintfs[0].rssi)
-    	time = time+1
-    	if time = 25
-    	
-   	 
-    
+    net.mobility(sta1, 'start', time=0, **p1)
+    net.mobility(sta2, 'start', time=0, **p2)
+    net.mobility(sta1, 'stop', time=25, **p3)
+    net.mobility(sta2, 'stop', time=25, **p4)
+    net.stopMobility(time=23)
+    print(sta1.wintfs[0].rssi)
+
     
     info("*** Starting Network ***\n")
     net.build()
@@ -80,4 +74,4 @@ def simple_file(args):
 
 if __name__ == '__main__':
     setLogLevel('info')
-    simple_file(sys.argv)
+    simple_file()
